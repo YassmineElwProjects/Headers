fdd94de51a6416efe9e8ed9b6e731fdd8b8f10e5
YassmineelwProjects <yassmelw@gmail.com>
2023-09-24 05:03:18 -0400

    Card Hover Part1

---
+++ Responsive Card Hover/script.js
@@ -0,0 +1,18 @@
+// Add this JavaScript to a file named script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    const cards = document.querySelectorAll(".card");
+  
+    // Function to close a card
+    function closeCard(card) {
+      card.classList.add("closed");
+    }
+  
+    // Add an event listener to close each card after 3 seconds
+    cards.forEach((card) => {
+      setTimeout(() => {
+        closeCard(card);
+      }, 3000); // 3000 milliseconds = 3 seconds
+    });
+  });
+  
