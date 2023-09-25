04c5964d42c7b3f781d572e5eb21fd2d41952306
YassmineelwProjects <yassmelw@gmail.com>
2023-09-25 01:18:44 -0400

    Final version

--- IntroWebSite/script.js
+++ IntroWebSite/script.js
@@ -3,32 +3,24 @@ let logoHeader = document.querySelector(".logo-header");
 let logoSpan = document.querySelectorAll(".logo");
 
 window.addEventListener("DOMContentLoaded", () => {
-  // Set the initial color to white
-  logoSpan.forEach((span) => {
-    span.style.color = "white";
-  });
-
   setTimeout(() => {
     logoSpan.forEach((span, idx) => {
       setTimeout(() => {
-        span.style.transform = "rotate(360deg)";
-        span.style.color = getRandomColor();
-        span.style.transition = "transform 1s ease-in-out, color 1s ease-in-out";
+        span.classList.add("active");
       }, (idx + 1) * 400);
     });
 
     setTimeout(() => {
-      intro.style.top = "-100vh";
+      logoSpan.forEach((span, idx) => {
+        setTimeout(() => {
+          span.classList.remove("active");
+          span.classList.add("fade");
+        }, (idx + 1) * 50);
+      });
     }, 2000);
 
+    setTimeout(() => {
+      intro.style.top = "-100vh";
+    }, 2400);
   });
-
-  function getRandomColor() {
-    const letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
 });
